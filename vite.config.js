75f0626219dc6e3ab5dc3acd0bd70215db30c6b7
Girish-Gaurav-Sharma <135326401+Girish-Gaurav-Sharma@users.noreply.github.com>
2024-10-13 23:44:40 +0530

    dsfg

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	build: {
+		outDir: 'dist', // Explicitly define the output directory
+		chunkSizeWarningLimit: 1000, // Optional: Increase chunk size limit if needed
+	},
+});
